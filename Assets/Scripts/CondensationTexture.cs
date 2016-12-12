0b59ab3f2d14b2802fbabbe55bd785d24adcad37
Jeremy Quentin <jeremy.quentin@gmail.com>
2016-12-11 19:36:28 -0500

    Texture switch

---
+++ Assets/Scripts/CondensationTexture.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CondensationTexture : MonoSingleton <CondensationTexture> 
+{
+
+	public List<Sprite> condensationSprites = new List<Sprite>();
+
+	int currentSpriteIndex = 0;
+
+	SpriteRenderer _spriteRenderer;
+	SpriteRenderer spriteRenderer
+	{
+		get
+		{
+			if (_spriteRenderer == null)
+				_spriteRenderer = GetComponent<SpriteRenderer>();
+			return _spriteRenderer;
+		}
+	}
+
+	void Start()
+	{
+		UpdateTexture();
+	}
+
+	public void SwitchTexture()
+	{
+		currentSpriteIndex = (currentSpriteIndex + 1) % condensationSprites.Count;
+		UpdateTexture();
+	}
+
+	void UpdateTexture()
+	{
+		spriteRenderer.sprite = condensationSprites[currentSpriteIndex];
+	}
+
+}
---
+++ Assets/Scripts/SwitchTextureButton.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchTextureButton : MonoBehaviour {
+
+	void OnClick () 
+	{
+		CondensationTexture.instance.SwitchTexture();
+	}
+
+}
